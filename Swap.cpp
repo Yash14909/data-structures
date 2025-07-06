6fe9ea9c0cf070d76bd3a984fa31e2bd307a31ee
Yashvardhan Reddy <yashvreddy2043@gmail.com>
2025-07-06 16:16:17 +0530

    Create Swap.cpp
    Add cpp file

---
+++ Swap.cpp
@@ -0,0 +1,32 @@
+// Pctl - 15:
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+void mySwap(T &a, T &b)
+{
+    T c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+int main()
+{
+
+    int i = 10, j = 20;
+    cout << "\ni = " << i << "\nj = " << j << endl;
+    cout << "\nAfter swapping" << endl;
+    mySwap(i, j);
+    cout << "\ni = " << i << "\nj = " << j << endl;
+
+    cout << "\nNew Entry...." << endl;
+    char ch = 'A', dh = 'Z';
+    cout << "\nch = " << ch << "\ndh = " << dh << endl;
+    cout << "\nAfter swapping" << endl;
+    mySwap(ch, dh);
+    cout << "\nch = " << ch << "\ndh = " << dh << endl;
+
+    return 0;
+}
